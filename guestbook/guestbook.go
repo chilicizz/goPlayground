1a0572fddfe78db6deb4d4ff506b6e0d69212bcf
Cyril Ng Lung Kit <nglkitc@yahoo.co.uk>
2020-08-01 13:31:51 +0800

    Completed guestbook

---
+++ guestbook/guestbook.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"fmt"
+	"html/template"
+	"bufio"
+	"os"
+)
+
+type Guestbook struct {
+	SignatureCount int
+	Signatures []string
+}
+
+func check(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func getStrings(filename string) []string {
+	var lines []string
+	file, err := os.Open(filename)
+	if os.IsNotExist(err) {
+		return nil
+	}
+	check(err)
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	check(scanner.Err())
+	return lines
+}
+
+func viewHandler(writer http.ResponseWriter, request *http.Request) {
+	signatures := getStrings("signatures.txt")
+	fmt.Printf("%#v\n", signatures)
+
+	html, err := template.ParseFiles("view.html")
+	check(err)
+	guestbook := Guestbook{
+		SignatureCount: len(signatures),
+		Signatures: signatures,
+	}
+	err = html.Execute(writer, guestbook)
+	check(err)
+}
+
+func newHandler(writer http.ResponseWriter, request *http.Request) {
+	html, err := template.ParseFiles("new.html")
+	check(err)
+	err = html.Execute(writer, nil)
+	check(err)
+}
+
+func createHandler(writer http.ResponseWriter, request *http.Request) {
+	signature := request.FormValue("signature")
+	options := os.O_WRONLY | os.O_APPEND | os.O_CREATE
+	file, err := os.OpenFile("signatures.txt", options, os.FileMode(0600))
+	check(err)
+	_, err = fmt.Fprintln(file, signature)
+	check(err)
+	err = file.Close()
+	check(err)
+	http.Redirect(writer, request, "/guestbook", http.StatusFound)
+}
+
+func main() {
+	host := ":8080"
+	fmt.Printf("Started server: %s", host)
+	http.HandleFunc("/guestbook", viewHandler)
+	http.HandleFunc("/guestbook/new", newHandler)
+	http.HandleFunc("/guestbook/create", createHandler)
+	err := http.ListenAndServe(host, nil)
+	log.Fatal(err)
+}
